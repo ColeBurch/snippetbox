37089467141e0b5943fbe0ace385bb683be087b7
ColeBurch <coleburch14@gmail.com>
2025-04-12 16:29:44 -0400

    implement toasts.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,15 +6,19 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/ColeBurch/snippetbox/internal/models"
+	"github.com/alexedwards/scs/postgresstore"
+	"github.com/alexedwards/scs/v2"
 	_ "github.com/lib/pq"
 )
 
 type application struct {
-	logger        *slog.Logger
-	snippets      *models.SnippetModel
-	templateCache map[string]*template.Template
+	logger         *slog.Logger
+	snippets       *models.SnippetModel
+	templateCache  map[string]*template.Template
+	sessionManager *scs.SessionManager
 }
 
 func main() {
@@ -35,10 +39,15 @@ func main() {
 		os.Exit(1)
 	}
 
+	sessionManager := scs.New()
+	sessionManager.Store = postgresstore.New(db)
+	sessionManager.Lifetime = 12 * time.Hour
+
 	app := &application{
-		logger:        logger,
-		snippets:      &models.SnippetModel{DB: db},
-		templateCache: templateCache,
+		logger:         logger,
+		snippets:       &models.SnippetModel{DB: db},
+		templateCache:  templateCache,
+		sessionManager: sessionManager,
 	}
 
 	logger.Info("starting server on :8000")
--- go.mod
+++ go.mod
@@ -4,4 +4,8 @@ go 1.24.0
 
 require github.com/lib/pq v1.10.9
 
-require github.com/justinas/alice v1.2.0
+require (
+	github.com/alexedwards/scs/postgresstore v0.0.0-20250212122300-421ef1d8611c
+	github.com/alexedwards/scs/v2 v2.8.0
+	github.com/justinas/alice v1.2.0
+)
